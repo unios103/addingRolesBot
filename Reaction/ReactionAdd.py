c5d1f08f62e11edf455d74f3692343d9926f601d
Noobest <thefackingnoobest@gmail.com>
2020-10-10 01:26:34 +0900

    ReactionAddを追加

---
+++ Reaction/ReactionAdd.py
@@ -0,0 +1,16 @@
+import discord
+from typing import Union
+
+
+
+class ReactionAdd:
+    def __init__(self):
+        pass
+
+    def handle(
+        self,
+        reaction: discord.Reaction,
+        user: Union[discord.Member, discord.User]
+    ):
+        #TODO
+        pass
--- main.py
+++ main.py
@@ -1,2 +1,27 @@
-if __name__ == "__main__":
-    pass
+import discord
+import os
+from typing import Union
+
+from Reaction.ReactionAdd import ReactionAdd
+
+
+
+class Client(discord.Client):
+    __TOKEN = os.environ["TOKEN"]
+
+
+    def run(self):
+        super().run(Client.__TOKEN)
+
+    async def on_ready(self):
+        self._reaction_add = ReactionAdd()
+
+    async def on_message(self, message: discord.Message):
+        pass
+
+    async def on_reaction_add(
+        self,
+        reaction: discord.Reaction,
+        user: Union[discord.Member, discord.User]
+    ):
+        self._reaction_add.handle(reaction, user)
