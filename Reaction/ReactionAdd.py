26823123a5a642ba9e5bb72ae897146411cfe262
Noobest <thefackingnoobest@gmail.com>
2020-10-10 01:42:19 +0900

    生きててごめんなさい…

--- Reaction/ReactionAdd.py
+++ Reaction/ReactionAdd.py
@@ -7,10 +7,9 @@ class ReactionAdd:
     def __init__(self):
         pass
 
-    def handle(
+    async def handle(
         self,
         reaction: discord.Reaction,
         user: Union[discord.Member, discord.User]
     ):
-        #TODO
-        pass
+        await reaction.message.channel.send("test")
