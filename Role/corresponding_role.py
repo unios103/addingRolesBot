bb11c944e7134c1c61d0054068af65056027c7b1
unios103 <unios103@gmail.com>
2020-10-11 01:56:29 +0900

    ロールの追加

---
+++ Role/corresponding_role.py
@@ -0,0 +1,25 @@
+import discord
+import re
+from typing import Optional
+
+import Role.role_command as rc
+
+def get_corresponding_role(message: discord.Message, emoji: discord.Emoji) -> Optional[discord.Role]:
+    contents = re.sub('( |\n)', '', message.content)
+
+    command_search = re.search('\+role', contents)
+    if command_search == None:
+        return None
+
+    contents = contents[command_search.end():]
+
+    if contents.find("=") == -1:
+        return rc.get_role(message)
+
+    match_emoji = contents.find(str(emoji))
+    if match_emoji == -1:
+        return None
+
+    role = contents[match_emoji:].split(",")[0].split("=")[1][3: -1]
+
+    return message.guild.get_role(int(role))
